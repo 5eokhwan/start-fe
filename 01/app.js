cec83d9e06d5d4831aeec90894939aee5cde61ef
5eokhwan <exist909@naver.com>
2021-03-11 19:13:09 +0900

    add 01

---
+++ 01/app.js
@@ -0,0 +1,19 @@
+console.log("app.js");
+
+image = [];
+
+image[0] = "1.jpg";
+image[1] = "2.jpg";
+
+var image = {};
+
+image.width = 1;
+image.height = 2;
+
+image["width"] = 1;
+
+image["name" + i] = "1.jpg";
+
+image = { width: 1, height: 2 };
+
+console.log(image);
